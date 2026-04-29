1abc009160deef857903d21a42bad72f25ad08df
Femeow Guy <femeownguy@gmail.com>
2026-04-28 23:12:04 -0700

    Add Enuo week-one anchor for Trials III

--- config/weekStartConfig.js
+++ config/weekStartConfig.js
@@ -4,6 +4,7 @@ export const weekStartAnchors = {
   "AAC Cruiserweight": "2025-04-01",
   "AAC Heavyweight": "2026-01-06",
   "Trials III (Extreme)::Doomtrain": "2025-12-16",
+  "Trials III (Extreme)::Enuo": "2026-04-28",
 };
 
 export default { weekStartAnchors };
